c26420b90fbe307a887594e6053a6aa88defceab
NhatRmit <62049702+NhatRmit@users.noreply.github.com>
2022-09-05 22:50:38 +0700

    section 4 done - update dir and cate list and cate item

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,19 @@
+import CategoryItem from '../category-item/category-item.component';
+import './directory.styles.scss';
+
+const Directory = (props) => {
+	const { categories } = props;
+
+	return (
+		<div className='categories-container'>
+			{categories.map((category) => (
+				<CategoryItem
+					key={category.id}
+					category={category}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default Directory;
